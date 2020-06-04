51657ef4ebb2dd7d20f9554ba72b5569d2838ada
Dwire <gjdwyer8@gmail.com>
2020-06-04 16:01:27 -0400

    donzo

---
+++ 11-intro-to-rails/new_rails_school/config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #VERB - URL    -  controller#action
+  get "/students", to: 'students#index'
+  get "/students/:id", to: 'students#show'
+    
+end
