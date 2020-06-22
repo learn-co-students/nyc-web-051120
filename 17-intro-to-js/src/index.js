ba5e29527a6597d3cdd5cdd8b65c40e5c0a8100b
Steven Doran <steven.doran@flatironschool.com>
2020-06-22 12:01:37 -0400

    post lecture

---
+++ 17-intro-to-js/src/index.js
@@ -0,0 +1,78 @@
+console.log("Welcome to Mod 3")
+
+/*
+ **Questions
+
+
+ **Why JS?
+
+  - three pillars
+
+  1. event handling - listening and responding to different types of user input
+  2. API requests (fetch) - making HTTP requests using JS
+  3. DOM manipulation/traversal - controlling what the user sees on the web page
+
+ 
+  - Single Page Applications
+
+
+ **What is JS?
+
+ - ECMAScript
+
+
+ **How JS?
+
+  - inline script tage
+  - load local js files
+  - load js files from the internet - CDN
+
+
+ **Some data types
+ - null
+
+  - equivalent to `nil` in Ruby
+  - indicates the absence of a value
+  - null is falsey
+ 
+ - Number
+
+  - only data type that handles numbers
+  - both decimals and integers
+  - beware of rounding errors
+ 
+ - String
+
+  - much like ruby
+  - "" ==> Ruby "#{variable} something something"
+  - ''
+  - `` ==> JS `${variable} something something`
+
+ - Object Literals
+
+  - equivalent to Ruby hashes
+  - key-value pairs
+  - can be accessed using square-bracket notion OR dot notation
+  - can hold multiple data types as values - numbers, strings, arrays, other objects etc.
+ 
+ - Array
+
+  - pretty much the same as Ruby
+  - can hold multiple data types
+  - 0-indexed
+  - fewer built in methods than Ruby
+
+ - undefined
+
+  - represents the presence of something that hasn't yet been defined
+
+ **Functions
+
+
+*/
+
+// function declaration, named function
+
+
+// function expression
+
