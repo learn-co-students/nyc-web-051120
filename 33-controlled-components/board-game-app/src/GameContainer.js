d17573ecd6acedf7ad9d5618f61cbebf2d3215d5
Caryn McCarthy <cmccarthy15@gmail.com>
2020-07-14 17:44:38 -0400

    after controlled conpomonets

--- 33-controlled-components/board-game-app/src/GameContainer.js
+++ 33-controlled-components/board-game-app/src/GameContainer.js
@@ -4,7 +4,9 @@ import NewGameForm from './NewGameForm'
 
 class GameContainer extends React.Component {
     state = {
-        games: []
+        games: [],
+        search: '',
+        maxPlayers: '0'
     }
 
     getAllGames = () => {
@@ -18,14 +20,36 @@ class GameContainer extends React.Component {
             })
     }
 
+    handleNewGame = (newGame) => {
+        // add the new game object into the current games in state
+        this.setState({ games: [...this.state.games, newGame] })
+    }
+
+    handleChange = e => this.setState({ [e.target.name]: e.target.value })
+
     render(){
         console.log('inside of GameContainer state', this.state)
+        // logic for only showing the gamecards who meet the criteria
+        let filteredGames = this.state.games.filter(game => 
+            game.name.toLowerCase().includes(this.state.search.toLowerCase())
+            && game.maxPlayers >= parseInt(this.state.maxPlayers)
+        )
+        // We can do this because... "any random string".includes("") === true 
+
         return (
             <div id="game-container">
-                <NewGameForm />
+                <NewGameForm handleNewGame={this.handleNewGame} />
                 <h2 onClick={this.getAllGames}>All Games</h2>
+                <label>
+                    Search: 
+                    <input name="search" placeholder="Search by game name..." onChange={this.handleChange} value={this.state.search}/>
+                </label>
+                <label>
+                    Search by Max Players: 
+                    <input type="number" name="maxPlayers" placeholder="Search by max players..." onChange={this.handleChange} value={this.state.maxPlayers}/>
+                </label>
                 <div id="game-list">
-                    {this.state.games.map((game, index) => 
+                    {filteredGames.map((game, index) => 
                         <GameCard 
                             key={index}
                             name={game.name}
--- 33-controlled-components/board-game-app/src/NewGameForm.js
+++ 33-controlled-components/board-game-app/src/NewGameForm.js
@@ -1,28 +1,107 @@
 import React from 'react'
 
-const NewGameForm = props => {
-    return (
-        <form id="new-game-form">
-            <h2>Create a New Game</h2>
-            <label>
-                Name:
-                <input placeholder="Name"/>
-            </label>
-            <label>
-                Min Age:
-                <input type="number" placeholder="Minimum Age"/>
-            </label>
-            <label>
-                Min Players:
-                <input type="number" placeholder="Minimum # Players"/>
-            </label>
-            <label>
-                Min Players:
-                <input type="number" placeholder="Maximum # Players"/>
-            </label>
-            <button type="submit">Submit</button>
-        </form>
-    )
+/**
+ * Controlled Form in readt
+ * 1. A piece of state for every input field 
+ * 2. A change handler for that input field, attached to the relevant input tag, that updates our state
+ * 3. The *value* attribute on the input itself **************
+ * 4. A submit handler on the form itself (if you've got a submit button)
+ * 
+ */
+
+const initialState = {
+    name: '',
+    minAge: 0,
+    minPlayers: 1,
+    maxPlayers: 1
+}
+class NewGameForm extends React.Component {
+    state = initialState
+
+    // DO NOT NEED FOUR IF WE HAVE NAMES ON THE INPUTS
+    // handleNameChange = (e) => {
+    //     this.setState({ name: e.target.value })
+    // }
+
+    // handleAgeChange = e => {
+    //     this.setState({ minAge: e.target.value })
+    // }
+
+    // handleMinPlayerChange = e => {
+    //     this.setState({ minPlayers: e.target.value })
+    // }
+
+    // handleMaxPLayerChange = e => {
+    //     this.setState({ maxPlayers: e.target.value })
+    // }
+
+    // handleClear = (e) => {
+    //     e.preventDefault()
+    //     this.setState({ name: '' })
+    // }
+
+    handleChange = e => {
+        console.log(e.target.name, '=======', e.target.value)
+        this.setState({ [e.target.name] : e.target.value })
+    }
+
+    handleSubmit = e => {
+        let { name, minAge, minPlayers, maxPlayers } = this.state
+        e.preventDefault() //keeps it from refreshing the page 
+        fetch('http://localhost:3000/games', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json'
+            },
+            body: JSON.stringify({
+                name, 
+                minAge: parseInt(minAge),
+                minPlayers: parseInt(minPlayers),
+                maxPlayers: parseInt(maxPlayers),
+                likes: 0, 
+                isCollaborative: false
+            })
+        })
+            .then(res => res.json())
+            .then(data => {
+                // MOD3 DON'T DO THIS ANYMORE!!!! e.target.reset()
+                // NEED TO CLEAR THE FORM
+                this.setState(initialState)
+
+                // NEED TO UPDATE THE FRONTEND DATA TO INCLUDE OUR NEW GAME
+                this.props.handleNewGame(data)
+                // console.log(data)
+            })
+    }
+
+
+    render(){
+        // console.log('NewGameForm State', this.state)
+        return (
+            <form id="new-game-form" onSubmit={this.handleSubmit}>
+                <h2>Create a New Game</h2>
+                {/* <button onClick={this.handleClear}>Clear</button> */}
+                <label>
+                    Name:
+                    <input name="name" placeholder="Name" onChange={e => this.handleChange(e)} value={this.state.name}/>
+                </label>
+                <label>
+                    Min Age:
+                    <input name="minAge" type="number" placeholder="Minimum Age"  onChange={this.handleChange} value={this.state.minAge}/>
+                </label>
+                <label>
+                    Min Players:
+                    <input name="minPlayers" type="number" placeholder="Minimum # Players" onChange={this.handleChange} value={this.state.minPlayers}/>
+                </label>
+                <label>
+                    Min Players:
+                    <input name="maxPlayers" type="number" placeholder="Maximum # Players" onChange={this.handleChange} value={this.state.maxPlayers}/>
+                </label>
+                <button type="submit">Submit</button>
+            </form>
+        )
+    }
 }
 
 export default NewGameForm
