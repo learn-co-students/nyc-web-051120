8ee4b925f4cc257c3c73a5fa4b96e8edc0fecf44
Caryn McCarthy <cmccarthy15@gmail.com>
2020-07-17 09:57:30 -0400

    potion shop solution

---
+++ 36-potion-shop-dq/src/Inventory.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import Purse from "./Purse"
+import InventoryTile from "./InventoryTile"
+import PotionTile from "./PotionTile"
+
+
+
+const Inventory = (props) => {
+
+  const renderInventory = () => {
+
+    return props.inventory.map(item => {
+      const potion = props.potions.find(potion => potion.id === item.id)
+
+       return (
+        <PotionTile potion={potion} potionAction={props.sell} divClassName="inventory-card">
+          <h4>{potion.name} x {item.amount}</h4>
+        </PotionTile>
+      )
+    })  
+  }
+
+  return (
+    <div className="inventory">
+      <h2>Inventory</h2>
+      <Purse stopShake={props.stopShake} shake={props.shake} gold={props.gold}/>
+      <div className="inventory-cards">
+       {renderInventory()}
+      </div>
+    </div>
+  )
+}
+
+export default Inventory
---
+++ 36-potion-shop-dq/src/Shop.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import ShopTile from "./ShopTile"
+import PotionTile from "./PotionTile"
+
+const Shop = (props) => {
+
+  const renderPotions = () => {
+    return props.potions.map(potion => {
+      return ( 
+        <PotionTile potion={potion} potionAction={props.addToInventory} divClassName="card">
+          <h4 className="potion-header">{potion.name}</h4>
+        </PotionTile>
+      )
+    })
+  }
+
+  return (
+    <div className="shop">
+      <div className="shop-header">
+        <img className="torch" src="https://img.itch.zone/aW1nLzEzNDg2ODkuZ2lm/original/BEjCgg.gif" alt="torch"/>
+        <h1>Ye Olde Apothecary</h1>
+        <img className="torch flipped" src="https://img.itch.zone/aW1nLzEzNDg2ODkuZ2lm/original/BEjCgg.gif" alt="torch"/>
+      </div>
+      <div className="shop-cards">
+        {renderPotions()}
+      </div>
+    </div>
+  )
+}
+
+export default Shop
+
